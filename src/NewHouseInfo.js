dba12a0376b1a706813b4bea932b19bd08a704d7
Leroy G <liguan698@gmail.com>
2023-06-28 19:41:27 -0700

    add house info on form

--- src/NewHouseInfo.js
+++ src/NewHouseInfo.js
@@ -1,12 +1,29 @@
 import React, { useState } from "react";
 
-function NewHouseInfo() {
+function NewHouseInfo(onAddHouse) {
   const [image, setImage] = useState("");
   const [price, setPrice] = useState("");
   const [year, setYear] = useState("");
+  const [bedrooms, setBedrooms] = useState("");
+  const [bathrooms, setBathrooms] = useState("");
+  const [size, setSize] = useState("");
 
-  function handleSubmit() {
-    console.log("form data");
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("http://localhost:3000/houses", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        image: image,
+        price: price,
+        year: year,
+        bedrooms: bedrooms,
+        bathrooms: bathrooms,
+        size: size,
+      }),
+    })
+      .then((r) => r.json())
+      .then((newHouse) => onAddHouse(newHouse));
   }
 
   return (
@@ -26,12 +43,27 @@ function NewHouseInfo() {
           value={price}
           onChange={(e) => setPrice(e.target.value)}
         />
+        <input type="text" name="size" placeholder="size" value={size} onChange={(e) => setSize(e.target.value)} />
         <input
           type="text"
           name="Year"
           placeholder="Year of Built"
           value={year}
           onChange={(e) => setYear(e.target.value)}
+        />
+        <input
+          type="text"
+          name="bedrooms"
+          placeholder="Number of Bedrooms"
+          value={bedrooms}
+          onChange={(e) => setBedrooms(e.target.value)}
+        />
+        <input
+          type="text"
+          name="Bathrooms"
+          placeholder="Number of Bathrooms"
+          value={bathrooms}
+          onChange={(e) => setBathrooms(e.target.value)}
         />
         <button type="submit">Add House</button>
       </form>
